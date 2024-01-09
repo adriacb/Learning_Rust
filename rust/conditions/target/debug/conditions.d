88d2d2d96c1e35d75d4d9ac28f95a95ca86b8e0f
Adrià <adrian.cabello@alumni.esci.upf.edu>
2024-01-09 22:21:29 +0100

    conditions

---
+++ rust/conditions/target/debug/conditions.d
@@ -0,0 +1,1 @@
+/media/adria/66EE6C40EE6C0B1F/shared/practice/programming/rust/conditions/target/debug/conditions: /media/adria/66EE6C40EE6C0B1F/shared/practice/programming/rust/conditions/src/main.rs
